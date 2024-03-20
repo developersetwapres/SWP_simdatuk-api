c0f275c2c57b94d75251dfb813fec34fdd749d61
osvaldosilitonga <osvaldosilitonga@gmail.com>
2024-03-20 15:35:29 +0700

    add: method updatePermissionAction dan findByRoleAndPermission

--- app/Repositories/RolePermissionRepository.php
+++ app/Repositories/RolePermissionRepository.php
@@ -7,6 +7,8 @@ use App\Models\RolePermission;
 interface RolePermissionRepositoryInterface
 {
     public function save(array $data);
+    public function updatePermissionAction(mixed $rolePermission, array $action);
+    public function findByRoleAndPermissionId(int $roleId, int $permissionId);
 }
 
 class RolePermissionRepository implements RolePermissionRepositoryInterface
@@ -14,5 +16,34 @@ class RolePermissionRepository implements RolePermissionRepositoryInterface
     public function save(array $data)
     {
         return RolePermission::insert($data);
+    }
+
+    public function updatePermissionAction(mixed $rolePermission, array $action)
+    {
+        if (isset($action['read']))
+        {
+            $rolePermission->read = $action['read'];
+        }
+        if (isset($action['create']))
+        {
+            $rolePermission->create = $action['create'];
+        }
+        if (isset($action['update']))
+        {
+            $rolePermission->update = $action['update'];
+        }
+        if (isset($action['delete']))
+        {
+            $rolePermission->delete = $action['delete'];
+        }
+
+        $rolePermission->save();
+
+        return $rolePermission;
+    }
+
+    public function findByRoleAndPermissionId(int $roleId, int $permissionId)
+    {
+        return RolePermission::where('permission_id', $permissionId)->where('role_id', $roleId)->first();
     }
 }
