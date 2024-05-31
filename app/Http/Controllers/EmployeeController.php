ac250e16cb744a5e1c1cdb1d912c8e79c306f426
Egi Nugraha <eginugrha@gmail.com>
2024-05-31 14:39:41 +0700

    fix: added sample for get list employee

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -90,7 +90,7 @@ class EmployeeController extends Controller
      * @queryParam page integer Refers to the current page of results being displayed. Default is '1'. Example: 1
      * @queryParam limit integer Refers to the maximum number of items to be displayed per page. Defaults is '10'. Example: 10
      * @queryParam search string The keyword search field for the name or employee id number. Example: administrator
-     * @queryParam type string Referst to the type of employee 1=ASN 2=NON ASN 3=OUTSOURCE.
+     * @queryParam type string Referst to the type of employee 1=ASN 2=NON ASN 3=OUTSOURCE. Example: 1
      * @response 200 {"code": 200, "message": "success", "data": [{"id": 32, "username": "admin", "employee_id_number": "0000000000000", "employee_registration_number": "0000000000000", "role_name": "administrator", "status": "Aktif"}], "pagination": {"total": 1, "count": 1, "per_page": 1, "current_page": 1, "total_pages": 1, "links": {"first_page": "http://localhost/api/users?page=1", "last_page": "http://localhost/api/users?page=1", "next_page": null, "prev_page": null}}}
      */
     public function index()
