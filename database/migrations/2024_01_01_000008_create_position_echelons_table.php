5f84cb75bd9d557507c443c306b789f1bac724d3
bayunvnt <bayunoviantoo9@gmail.com>
2024-06-06 20:18:29 +0700

    feat: add seeder jabatan fungsional #2

--- database/migrations/2024_01_01_000008_create_position_echelons_table.php
+++ database/migrations/2024_01_01_000008_create_position_echelons_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('positions');
+        Schema::dropIfExists('position_echelons');
     }
 };
