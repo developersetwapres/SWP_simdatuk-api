4fba324eeac5dff10eb22c35c20fdc41ba50a19d
Egi Nugraha <eginugrha@gmail.com>
2024-06-07 23:09:32 +0700

    fix: remove relation between position history with echelon

--- app/Http/Controllers/PositionHistoryController.php
+++ app/Http/Controllers/PositionHistoryController.php
@@ -29,7 +29,7 @@ class PositionHistoryController extends Controller
      * @queryParam page integer Refers to the current page of results being displayed. Default is '1'. Example: 1
      * @queryParam limit integer Refers to the maximum number of items to be displayed per page. Defaults is '10'. Example: 10
      * @queryParam search string The keyword search field for the name. Example: Asisten Wakil Presiden
-     * @response 200
+     * @response 200 {"code": 200,"message": "success","data": [{"id": 1,"name": "Perubahan Jabatan Mei ","period_month": 5,"period_year": "2024","created_at": "2024-06-07 10:22:28","total": 268}],"pagination": {"total": 292,"count": 10,"per_page": 10,"current_page": 1,"total_pages": 30,"links": {"first_page": "http://localhost/api/position-histories?page=1","last_page": "http://localhost/api/position-histories?page=30","next_page": "http://localhost/api/position-histories?page=2","prev_page": null}}}
      */
     public function index()
     {
@@ -103,7 +103,7 @@ class PositionHistoryController extends Controller
      * @authenticated
      * @urlParam id Refers to the ID of Position. Example: 1
      * @response 404 {"code": 404,"message": "Riwayat jabatan tidak ditemukan.","data": null}
-     * @response 200
+     * @response 200 {"code": 200,"message": "success","data": {"id": 1,"period_month": 5,"period_year": "2024","name": "Perubahan Jabatan Mei ","created_at": "2024-06-07 10:22:28","users": [{"id": 1,"user_id": 156,"name": "Mujiyono","employee_id_number": "195809031979031001","position": "Staf pada Pembantu Asisten Wakil Presiden Bidang Monitoring dan Kontrol Masyarakat, Asisten Wakil Presiden Bidang Pengawasan (th.1979-1984)","group_id": null,"group_name": null,"echelon": null,"position_status": 2,"effective_date": null,"decree": null,"decree_document": null,"decree_number": null,"type_decree_id": null,"type_decree_name": null,"type_termination_decree_id": null,"type_termination_decree_name": null,"decree_date": null,"termination_date": null,"termination_decree": null,"termination_decree_number": null,"termination_decree_date": null,"status": 0}]}}
      */
     public function show()
     {
@@ -119,7 +119,6 @@ class PositionHistoryController extends Controller
         $users = DB::table('position_history_users as phu');
         $users->join('users as u', 'u.id', '=', 'phu.user_id');
         $users->leftjoin('groups as g', 'phu.group_id', '=', 'g.id');
-        $users->leftjoin('echelons as e', 'phu.echelon_id', '=', 'e.id');
         $users->leftjoin('decrees as tod', 'phu.type_of_decree', '=', 'tod.id');
         $users->leftjoin('decrees as totd', 'phu.type_of_termination_decree', '=', 'totd.id');
         $users->where('phu.position_history_id', $positionHistory->id);
@@ -131,8 +130,7 @@ class PositionHistoryController extends Controller
             'phu.position',
             'g.id as group_id',
             'g.name as group_name',
-            'e.id as echelon_id',
-            'e.name as echelon_name',
+            'phu.echelon',
             'phu.position_status',
             'phu.effective_date',
             'phu.decree',
