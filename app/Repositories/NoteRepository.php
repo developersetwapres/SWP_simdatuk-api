f49407ea052d32b63f1cac6314d8286dca411d5a
bayunvnt <bayunoviantoo9@gmail.com>
2024-06-27 13:22:03 +0700

    fix: add missing response notes created_at and giver_name on detail employee

--- app/Repositories/NoteRepository.php
+++ app/Repositories/NoteRepository.php
@@ -8,13 +8,16 @@ class NoteRepository
 {
     public function getDetail($userId)
     {
-        $notes = DB::table('user_notes');
-        $notes->where('user_id', $userId);
-        $notes->select(
-            'id',
-            'description',
-        );
-        $notes->orderBy('created_at', 'desc');
+        $notes = DB::table('user_notes as un')
+            ->leftJoin('users as u', 'un.giver_id', '=', 'u.id')
+            ->where('un.user_id', $userId)
+            ->select(
+                'un.id',
+                'un.description',
+                'u.name as giver_name',
+                'un.created_at',
+            )
+            ->orderBy('un.created_at', 'desc');
         return $notes = $notes->get();
     }
 }
