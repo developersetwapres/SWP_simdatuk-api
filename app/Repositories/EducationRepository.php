95076266dd2501b0505e9d39f560f9092059a654
bayunvnt <bayunoviantoo9@gmail.com>
2024-06-28 18:32:22 +0700

    fix: add degree document response on detail employee

--- app/Repositories/EducationRepository.php
+++ app/Repositories/EducationRepository.php
@@ -2,26 +2,35 @@
 
 namespace App\Repositories;
 
+use App\Helpers\Document;
 use Illuminate\Support\Facades\DB;
 
 class EducationRepository
 {
+    use Document;
+
     public function getDetail($userId)
     {
-        $educations = DB::table('user_educations');
-        $educations->where('user_id', $userId);
-        $educations->select(
-            'id',
-            'level',
-            'name',
-            'faculty',
-            'major',
-            'status',
-            'year_of_graduation',
-            'description'
-        );
-        $educations->orderBy('level', 'desc');
-        $educations->orderBy('year_of_graduation', 'desc');
-        return $educations = $educations->get();
+        $educations = DB::table('user_educations')
+            ->where('user_id', $userId)
+            ->select(
+                'id',
+                'level',
+                'name',
+                'faculty',
+                'major',
+                'status',
+                'year_of_graduation',
+                'description',
+                'degree_document',
+            )
+            ->orderBy('level', 'desc')
+            ->orderBy('year_of_graduation', 'desc')->get();
+
+
+        foreach ($educations as $education) {
+            $education->degree_document = $this->getDocument($education->degree_document);
+        }
+        return $educations;
     }
 }
