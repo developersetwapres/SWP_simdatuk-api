f8c90e28c50e1eb5abad276a0d244794481c33f9
bayunvnt <bayunoviantoo9@gmail.com>
2024-06-28 13:12:48 +0700

    fix: nullable param on create employee #2

--- app/Http/Requests/Education/CreateEducationRequest.php
+++ app/Http/Requests/Education/CreateEducationRequest.php
@@ -20,7 +20,7 @@ class CreateEducationRequest
             'educations.*.status' => 'required|numeric|in:1,2,3,4,5',
             'educations.*.year_of_graduation' => 'required|date_format:Y',
             'educations.*.description' => 'max:160',
-            'educations.*.degree_document' => 'file|extensions:jpg,jpeg,png,pdf|max:2048',
+            'educations.*.degree_document' => 'nullable|file|extensions:jpg,jpeg,png,pdf|max:2048',
         ];
     }
 
