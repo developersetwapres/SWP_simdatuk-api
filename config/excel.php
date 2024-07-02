051ab444e03c761845b07c8d94962b56cf13b5bf
Egi Nugraha <eginugrha@gmail.com>
2024-07-03 02:43:27 +0700

    feat: change export csv with delimiter to ;

--- config/excel.php
+++ config/excel.php
@@ -13,14 +13,14 @@ return [
         | When using FromQuery, the query is automatically chunked.
         | Here you can specify how big the chunk should be.
         |
-        */
-        'chunk_size'             => 1000,
+         */
+        'chunk_size' => 1000,
 
         /*
         |--------------------------------------------------------------------------
         | Pre-calculate formulas during export
         |--------------------------------------------------------------------------
-        */
+         */
         'pre_calculate_formulas' => false,
 
         /*
@@ -30,7 +30,7 @@ return [
         |
         | When enabling strict null comparison empty cells ('') will
         | be added to the sheet.
-        */
+         */
         'strict_null_comparison' => false,
 
         /*
@@ -40,16 +40,16 @@ return [
         |
         | Configure e.g. delimiter, enclosure and line ending for CSV exports.
         |
-        */
-        'csv'                    => [
-            'delimiter'              => ',',
-            'enclosure'              => '"',
-            'line_ending'            => PHP_EOL,
-            'use_bom'                => false,
+         */
+        'csv' => [
+            'delimiter' => ';',
+            'enclosure' => '"',
+            'line_ending' => PHP_EOL,
+            'use_bom' => false,
             'include_separator_line' => false,
-            'excel_compatibility'    => false,
-            'output_encoding'        => '',
-            'test_auto_detect'       => true,
+            'excel_compatibility' => false,
+            'output_encoding' => '',
+            'test_auto_detect' => true,
         ],
 
         /*
@@ -59,21 +59,21 @@ return [
         |
         | Configure e.g. default title, creator, subject,...
         |
-        */
-        'properties'             => [
-            'creator'        => '',
+         */
+        'properties' => [
+            'creator' => '',
             'lastModifiedBy' => '',
-            'title'          => '',
-            'description'    => '',
-            'subject'        => '',
-            'keywords'       => '',
-            'category'       => '',
-            'manager'        => '',
-            'company'        => '',
+            'title' => '',
+            'description' => '',
+            'subject' => '',
+            'keywords' => '',
+            'category' => '',
+            'manager' => '',
+            'company' => '',
         ],
     ],
 
-    'imports'            => [
+    'imports' => [
 
         /*
         |--------------------------------------------------------------------------
@@ -85,8 +85,8 @@ return [
         | however if you want to do some logic based on style data
         | you can enable it by setting read_only to false.
         |
-        */
-        'read_only'    => true,
+         */
+        'read_only' => true,
 
         /*
         |--------------------------------------------------------------------------
@@ -98,7 +98,7 @@ return [
         | containing empty strings or empty values are not ignored but can be
         | ignored by enabling the setting ignore_empty to true.
         |
-        */
+         */
         'ignore_empty' => false,
 
         /*
@@ -109,8 +109,8 @@ return [
         | Configure the heading row formatter.
         | Available options: none|slug|custom
         |
-        */
-        'heading_row'  => [
+         */
+        'heading_row' => [
             'formatter' => 'slug',
         ],
 
@@ -121,13 +121,13 @@ return [
         |
         | Configure e.g. delimiter, enclosure and line ending for CSV imports.
         |
-        */
-        'csv'          => [
-            'delimiter'        => null,
-            'enclosure'        => '"',
+         */
+        'csv' => [
+            'delimiter' => null,
+            'enclosure' => '"',
             'escape_character' => '\\',
-            'contiguous'       => false,
-            'input_encoding'   => 'UTF-8',
+            'contiguous' => false,
+            'input_encoding' => 'UTF-8',
         ],
 
         /*
@@ -137,28 +137,28 @@ return [
         |
         | Configure e.g. default title, creator, subject,...
         |
-        */
-        'properties'   => [
-            'creator'        => '',
+         */
+        'properties' => [
+            'creator' => '',
             'lastModifiedBy' => '',
-            'title'          => '',
-            'description'    => '',
-            'subject'        => '',
-            'keywords'       => '',
-            'category'       => '',
-            'manager'        => '',
-            'company'        => '',
+            'title' => '',
+            'description' => '',
+            'subject' => '',
+            'keywords' => '',
+            'category' => '',
+            'manager' => '',
+            'company' => '',
         ],
 
         /*
-       |--------------------------------------------------------------------------
-       | Cell Middleware
-       |--------------------------------------------------------------------------
-       |
-       | Configure middleware that is executed on getting a cell value
-       |
-       */
-        'cells'        => [
+        |--------------------------------------------------------------------------
+        | Cell Middleware
+        |--------------------------------------------------------------------------
+        |
+        | Configure middleware that is executed on getting a cell value
+        |
+         */
+        'cells' => [
             'middleware' => [
                 //\Maatwebsite\Excel\Middleware\TrimCellValue::class,
                 //\Maatwebsite\Excel\Middleware\ConvertEmptyCellValuesToNull::class,
@@ -175,23 +175,23 @@ return [
     | Configure here which writer/reader type should be used when the package
     | needs to guess the correct type based on the extension alone.
     |
-    */
+     */
     'extension_detector' => [
-        'xlsx'     => Excel::XLSX,
-        'xlsm'     => Excel::XLSX,
-        'xltx'     => Excel::XLSX,
-        'xltm'     => Excel::XLSX,
-        'xls'      => Excel::XLS,
-        'xlt'      => Excel::XLS,
-        'ods'      => Excel::ODS,
-        'ots'      => Excel::ODS,
-        'slk'      => Excel::SLK,
-        'xml'      => Excel::XML,
+        'xlsx' => Excel::XLSX,
+        'xlsm' => Excel::XLSX,
+        'xltx' => Excel::XLSX,
+        'xltm' => Excel::XLSX,
+        'xls' => Excel::XLS,
+        'xlt' => Excel::XLS,
+        'ods' => Excel::ODS,
+        'ots' => Excel::ODS,
+        'slk' => Excel::SLK,
+        'xml' => Excel::XML,
         'gnumeric' => Excel::GNUMERIC,
-        'htm'      => Excel::HTML,
-        'html'     => Excel::HTML,
-        'csv'      => Excel::CSV,
-        'tsv'      => Excel::TSV,
+        'htm' => Excel::HTML,
+        'html' => Excel::HTML,
+        'csv' => Excel::CSV,
+        'tsv' => Excel::TSV,
 
         /*
         |--------------------------------------------------------------------------
@@ -201,8 +201,8 @@ return [
         | Configure here which Pdf driver should be used by default.
         | Available options: Excel::MPDF | Excel::TCPDF | Excel::DOMPDF
         |
-        */
-        'pdf'      => Excel::DOMPDF,
+         */
+        'pdf' => Excel::DOMPDF,
     ],
 
     /*
@@ -221,12 +221,12 @@ return [
     | [x] PhpOffice\PhpSpreadsheet\Cell\StringValueBinder::class
     | [x] PhpOffice\PhpSpreadsheet\Cell\AdvancedValueBinder::class
     |
-    */
-    'value_binder'       => [
+     */
+    'value_binder' => [
         'default' => Maatwebsite\Excel\DefaultValueBinder::class,
     ],
 
-    'cache'        => [
+    'cache' => [
         /*
         |--------------------------------------------------------------------------
         | Default cell caching driver
@@ -242,8 +242,8 @@ return [
         |
         | Drivers: memory|illuminate|batch
         |
-        */
-        'driver'      => 'memory',
+         */
+        'driver' => 'memory',
 
         /*
         |--------------------------------------------------------------------------
@@ -254,8 +254,8 @@ return [
         | persist to the store when the memory limit is reached.
         | Here you can tweak the memory limit to your liking.
         |
-        */
-        'batch'       => [
+         */
+        'batch' => [
             'memory_limit' => 60000,
         ],
 
@@ -270,8 +270,8 @@ return [
         | You can use any store defined in your cache config. When leaving
         | at "null" it will use the default store.
         |
-        */
-        'illuminate'  => [
+         */
+        'illuminate' => [
             'store' => null,
         ],
 
@@ -304,10 +304,10 @@ return [
     |
     | Supported handlers: null|db
     |
-    */
+     */
     'transactions' => [
         'handler' => 'db',
-        'db'      => [
+        'db' => [
             'connection' => null,
         ],
     ],
@@ -324,8 +324,8 @@ return [
         | permissions is an array with the permission flags for the directory (dir)
         | and the create file (file).
         |
-        */
-        'local_path'          => storage_path('framework/cache/laravel-excel'),
+         */
+        'local_path' => storage_path('framework/cache/laravel-excel'),
 
         /*
         |--------------------------------------------------------------------------
@@ -336,8 +336,8 @@ return [
         | and the create file (file).
         | If omitted the default permissions of the filesystem will be used.
         |
-        */
-        'local_permissions'   => [
+         */
+        'local_permissions' => [
             // 'dir'  => 0755,
             // 'file' => 0644,
         ],
@@ -355,9 +355,9 @@ return [
         | the local path. This setting only has effect when using
         | in conjunction with queued imports and exports.
         |
-        */
-        'remote_disk'         => null,
-        'remote_prefix'       => null,
+         */
+        'remote_disk' => null,
+        'remote_prefix' => null,
 
         /*
         |--------------------------------------------------------------------------
@@ -373,7 +373,7 @@ return [
         | queued chunk is processed the local temporary file is deleted on the server that
         | processed it.
         |
-        */
+         */
         'force_resync_remote' => null,
     ],
 ];
