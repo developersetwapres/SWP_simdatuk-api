8f06037b8f71b96a1111959874490a8df228352c
Agung Hasbi <agunghasbi13@gmail.com>
2024-07-03 14:10:57 +0700

    feat: import excel for add bulk employee #4

--- database/migrations/2024_07_03_061448_create_activity_log_table.php
+++ database/migrations/2024_07_03_061448_create_activity_log_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('activity_log');
+        Schema::dropIfExists('activity_logs');
     }
 };
