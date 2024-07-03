40831284cab7d18e847f9bbb0ce0ad764bdea01e
Agung Hasbi <agunghasbi13@gmail.com>
2024-07-03 14:04:38 +0700

    feat: import excel for add bulk employee #3

--- routes/api.php
+++ routes/api.php
@@ -103,7 +103,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('/{id}', [EmployeeController::class, 'update']);
         Route::put('/status', [EmployeeController::class, 'status']);
         Route::patch('/synchronization', [SynchronizationController::class, 'index']);
-        Route::get('import/download-template/{type}', [ImportEmployeeController::class, 'downloadTemplate']);
+        Route::get('/import/download-template/{type}', [ImportEmployeeController::class, 'downloadTemplate']);
         Route::get('/import/histories', [ImportEmployeeController::class, 'getRiwayatImport']);
     });
 
