9f1fba60c9821c1bfaa2d0ad6e9296918fc6bad9
Egi Nugraha <eginugrha@gmail.com>
2024-08-06 13:08:02 +0700

    fix: added jns_kepeg TNI to employment_type_id 1

--- database/seeders/OldUserSeeder.php
+++ database/seeders/OldUserSeeder.php
@@ -72,6 +72,7 @@ class OldUserSeeder extends Seeder
                     END AS marital_status,
                     CASE
                         WHEN jns_kepeg = 'TNI/POLRI' THEN '1'
+                        WHEN jns_kepeg = 'TNI' THEN '1'
                         WHEN jns_kepeg = 'Sipil' THEN '2'
                         WHEN jns_kepeg = 'Organik' THEN '3'
                         WHEN jns_kepeg = 'PPPK' THEN '4'
