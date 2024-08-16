df1686708ad0c75ce21e40df2013ce513dd7d07e
Egi Nugraha <eginugrha@gmail.com>
2024-08-16 16:56:16 +0700

    fix: remove migration.sql

--- migration.sql
+++
@@ -1,6 +0,0 @@
--- Migration data from old database to new database
-
--- Insert ASN user
-INSERT INTO simdatuk.users (name, created_at)
-SELECT nm_pegawai, CURRENT_TIMESTAMP
-FROM simdatuk_dump.tbl_1pegawai_swp;
